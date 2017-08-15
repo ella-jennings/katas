513abb81d09e9d19eec1944fe0176fee8b7f894e
Ella Jennings <ella.jennings@codecomputerlove.com>
2017-08-15 16:34:45 +0100

    chore: wrote first failing test

--- string-calculator/string-calculator-kata/string-calculator-kata/UnitTest1.cs
+++ string-calculator/string-calculator-kata/string-calculator-kata/UnitTest1.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
-
+using Assert = NUnit.Framework.Assert;
 
 namespace string_calculator_kata
 {
-    [Test]
+    [TestClass]
     public class UnitTest1
     {
         [Test]
         public void EmptyStringShouldReturn0()
         {
+            Assert.Fail();
         }
     }
 }
