0cff2031cff54223ca62fb3cfee0f328cad4ae2b
Ella Jennings <ella.jennings@codecomputerlove.com>
2017-08-15 16:17:30 +0100

    first commit - created file

---
+++ string-calculator/string-calculator-kata/string-calculator-kata/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace string_calculator_kata
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
