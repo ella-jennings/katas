bae2482169cd1f56dd7ad51c8571f56e825e7c73
Ella Jennings <20407096+ella-jennings@users.noreply.github.com>
2017-08-22 14:29:37 +0100

    Pass - negative numbers not allowed test. Fixed previous void test

--- string-calculator/string-calculator-kata/string-calculator-kata/UnitTest1.cs
+++ string-calculator/string-calculator-kata/string-calculator-kata/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using Assert = NUnit.Framework.Assert;
@@ -9,31 +11,34 @@ namespace string_calculator_kata
     [TestClass]
     public class StringCalculatorTests
     {
-        [TestCase("", 0)]
-        [TestCase("1", 1)]
-        [TestCase("1,2", 3)]
-        [TestCase("1,2,3", 6)]
-        [TestCase("1\n2",3)]
-        [TestCase("1,2\n3", 6)]
-        [TestCase("-1"), "Negative numbers not allowed: -1"]
-        public void InputShouldReturnExpectedOutput(string expectedInput, int expectedOutput)
+        [TestCase("", "0")]
+        [TestCase("1", "1")]
+        [TestCase("1,2", "3")]
+        [TestCase("1,2,3", "6")]
+        [TestCase("1\n2","3")]
+        [TestCase("1,2\n3", "6")]
+        [TestCase("-1", "Negative numbers not allowed: -1")]
+   
+
+        public void InputShouldReturnExpectedOutput(string expectedInput, string expectedOutput)
         {
             string stringOfNumbers = expectedInput;
 
-            int stringSum = new StringCalculator().Add(stringOfNumbers);
+            string stringSum = new StringCalculator().Add(stringOfNumbers);
 
             Assert.That(stringSum, Is.EqualTo(expectedOutput));
         }
         
         public class StringCalculator
         {
-            public int Add(string stringOfNumbers)
+            public string Add(string stringOfNumbers)
             {
                    if (string.IsNullOrEmpty(stringOfNumbers))
-                    return 0;
-                else
+                    return "0";
+
                 {
-                    var result = 0;
+                    var result = "";
+                    var sum = 0;
                     string[] stringArray =  stringOfNumbers.Split(',','\n') ;
 
                     foreach (string number in stringArray)
@@ -41,13 +46,21 @@ namespace string_calculator_kata
                         var stringNumber = Convert.ToInt32(number);
                         if (stringNumber < 1)
                         {
-                            Console.WriteLine("Negative numbers not allowed: -1");
+                            result = ThrowException();
+                            return result;
                         }
-                        result += stringNumber;
+                        sum += stringNumber;
                     }
-              
+
+                    result = sum.ToString();
                     return result;
                 }
+            }
+
+            private string ThrowException()
+            {
+                string result = "Negative numbers not allowed: -1";
+                return result;
             }
         }
     }
