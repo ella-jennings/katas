0f982e0407ecb4c6ef86efd96e4596884a0e5a61
Ella Jennings <20407096+ella-jennings@users.noreply.github.com>
2017-09-26 15:55:06 +0100

    First commit with initial passing tests

---
+++ PotterKata/PotterKata/PotterKataTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace PotterKata
+{
+    [TestFixture]
+    public class PotterKataTests
+    {
+       [Test]
+        public void OneBookCostsEightEuros()
+        {
+            var book = new Book();
+
+            Assert.AreEqual(8, book.Price);
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(1, 8)]
+        [TestCase(2, 16)]
+        public void NBooksCostExpectedPrice(int numberOfBooks, int expectedPrice)
+        {
+            var basket = new Basket();
+            basket.GetTotal(numberOfBooks);
+
+            Assert.AreEqual(expectedPrice, basket.Total);
+        }
+    }
+
+    public class Basket
+    {
+        public int Total;
+
+        public void GetTotal(int numberOfBooks)
+        {
+            var book = new Book();
+            Total = numberOfBooks * book.Price;
+        }
+    }
+
+    public class Book
+    {
+        public readonly int Price = 8;
+    }
+}
