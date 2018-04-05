0f14594fff1dc52d8a114c711843aca1c304d3d5
Ella Jennings <20407096+ella-jennings@users.noreply.github.com>
2018-04-05 16:25:42 +0100

    feat: removed unused references to int

--- CheckoutConnaisance/CheckoutConnaisance/Checkout.cs
+++ CheckoutConnaisance/CheckoutConnaisance/Checkout.cs
@@ -15,18 +15,14 @@ namespace CheckoutConnaisance
       _expectedItemBCost = 30;
       _itemA = "a";
       _itemB = "b";
-      var expectedPrices = new Dictionary<string, int>
-      {
-        {_itemA, _expectedItemACost},
-        {_itemB, _expectedItemBCost}
-      };
+
       var expectedPricesAsMoney = new Dictionary<string, Money>
       {
         {_itemA, new Money(_expectedItemACost)},
         {_itemB, new Money(_expectedItemBCost)}
       };
 
-      _checkout = new Checkout(expectedPrices, expectedPricesAsMoney);
+      _checkout = new Checkout(expectedPricesAsMoney);
     }
 
     private Checkout _checkout;
@@ -40,7 +36,6 @@ namespace CheckoutConnaisance
     {
       _checkout.AddItemAsMoney(_itemA);
 
-      _checkout.GetTotal();
       var result = _checkout.GetTotalFromMoney();
 
       Assert.That(result.Value, Is.EqualTo(_expectedItemACost));
@@ -73,26 +68,13 @@ namespace CheckoutConnaisance
 
   internal class Checkout
   {
-    private readonly Dictionary<string, int> _itemPrices;
     private readonly Dictionary<string, Money> _itemPricesAsMoney;
-    private int _total;
     private Money _monetaryValue;
 
-    public Checkout(Dictionary<string, int> itemPrices, Dictionary<string, Money> itemPricesAsMoney)
+    public Checkout(Dictionary<string, Money> itemPricesAsMoney)
     {
       _monetaryValue = new Money(0);
-      _itemPrices = itemPrices;
       _itemPricesAsMoney = itemPricesAsMoney;
-    }
-
-    public int GetTotal()
-    {
-      return _total;
-    }
-
-    public void AddItem(string item)
-    {
-      _total = _itemPrices[item];
     }
 
     public Money GetTotalFromMoney()
